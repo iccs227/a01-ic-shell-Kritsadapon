87689cbe33802807f288cb3fe0b8829131d36771
Kritsadapon <76818934+Kritsadapon@users.noreply.github.com>
2025-05-08 08:26:49 +0000

    creating new process and excute the command within PATH env

--- icsh.c
+++ icsh.c
@@ -6,6 +6,9 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_CMD_BUFFER 255
 
@@ -68,7 +71,25 @@ void com_exe_cmd(char *buffer, char *l_cmd, int *exit_code) {
         } else if (strncmp(buffer, "echo ", 5) == 0) {
             printf("%s\n", buffer + 5);
         }else {
-            printf("bad command\n");
+            int pid = fork();
+            if (pid == 0){
+                char *args[MAX_CMD_BUFFER];
+                int i = 0;
+                char *token = strtok(buffer, " ");
+                while (token != NULL) {
+                    args[i++] = token;
+                    token = strtok(NULL, " ");
+                }
+                args[i] = NULL;
+                
+                if (execvp(args[0], args) == -1) {
+                    perror("Error");
+                    exit(1);
+                }
+            } else{
+                int status;
+                waitpid(pid, &status, 0);
+            }
         }
 }
 
